267c635dc698e551b05d3b1ac5bd7fb5cca5ffba
Xu Haozhe <jamesxuhaozhe@gmail.com>
2020-06-30 22:32:14 +0800

    initial commit

---
+++ cmake-build-debug/CMakeFiles/CPrimerPlusReading.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPrimerPlusReading.dir/main.c.o"
+  "CPrimerPlusReading"
+  "CPrimerPlusReading.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CPrimerPlusReading.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
